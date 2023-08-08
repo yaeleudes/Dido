db7e57cf2bc675a2ccba487d4b6de33198e1d515
Ayesh26 <77013111+yaeleudes@users.noreply.github.com>
2023-08-08 15:30:52 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
